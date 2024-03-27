8134c1c198c6b33b6ca74781e449b6c46f9e0d58
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2024-03-27 13:30:32 -0700

    chore(*): added ci

--- envs.ts
+++ envs.ts
@@ -4,8 +4,8 @@ type Envs = {
 	PORT: number;
 };
 
-function getEnv<T extends keyof Envs>(key: T): string {
-	const value = process.env[key];
+function getEnv<T extends keyof Envs>(key: T, defaultValue?: string): string {
+	const value = process.env[key] ?? defaultValue;
 	if (!value) {
 		throw new EnvNotFoundError(key);
 	}
@@ -13,5 +13,5 @@ function getEnv<T extends keyof Envs>(key: T): string {
 }
 
 export const envs: Envs = {
-	PORT: Number.parseInt(getEnv("PORT")),
+	PORT: Number.parseInt(getEnv("PORT", "3000")),
 };
