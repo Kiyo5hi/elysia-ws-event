36ea57d1ecbdb9753aeacad7e1dfb902113b326d
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2024-03-27 13:59:13 -0700

    chore(schemas): tweaks to the schema generator

--- scripts/generateSchemas.ts
+++ scripts/generateSchemas.ts
@@ -1,5 +1,10 @@
 import { TypeScriptToTypeBox } from "@sinclair/typebox-codegen";
 
-const types = await Bun.file("./types.ts").text();
+if (Bun.argv.length < 3) {
+    console.error("Usage: bun run scripts/generateSchemas.ts <path/to/types.ts>");
+    process.exit(1);
+}
+
+const types = await Bun.file(Bun.argv.at(-1) as string).text();
 const schemas = TypeScriptToTypeBox.Generate(types);
 console.log(schemas);
